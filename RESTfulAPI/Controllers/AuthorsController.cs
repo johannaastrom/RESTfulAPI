9800209791d813ec9a4ac3604dc9a7b5d507192c
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-06-19 11:35:08 +0200

    initial

---
+++ RESTfulAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using RESTfulAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTfulAPI.Controllers
+{
+	public class AuthorsController : Controller
+    {
+		private ILibraryRepository _libraryRepository;
+
+		public AuthorsController(ILibraryRepository libraryRepository)
+		{
+			_libraryRepository = libraryRepository;
+		}
+
+		public IActionResult GetAuthors()
+		{
+			var authorsFromRepo = _libraryRepository.GetAuthors();
+			return new JsonResult(authorsFromRepo);
+		}
+	}
+}
--- RESTfulAPI/Startup.cs
+++ RESTfulAPI/Startup.cs
@@ -4,47 +4,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using RESTfulAPI.Services;
+using RESTfulAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace RESTfulAPI
+namespace Library.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // register the DbContext on the container, getting the connection string from
+            // appSettings (note: use this during development; in a production environment,
+            // it's better to store the connection string in an environment variable)
+            var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            // register the repository
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            ILoggerFactory loggerFactory, LibraryContext libraryContext)
         {
+            loggerFactory.AddConsole();
+
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler();
             }
 
-            app.UseStaticFiles();
+            libraryContext.EnsureSeedDataForContext();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc(); 
         }
     }
 }
